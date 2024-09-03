b6eb35ec07ef1e5fcbea9b4f5a1efd58e74fd470
Lakshan Niroshan <147984650+Niroshan-k@users.noreply.github.com>
2024-09-03 20:13:08 +0530

    Add files via upload

---
+++ type2audio.py
@@ -0,0 +1,32 @@
+'''from playsound import playsound
+from gtts import gTTS
+
+def playaudio(audio):
+    playsound(audio)
+
+def convert_to_audio(text):
+    audio = gTTS(text=text, lang='en')
+    audio.save("textaudio.mp3")
+    playaudio("textaudio.mp3")
+
+convert_to_audio("Hello world, my name is Lakshan.")'''
+
+import pygame
+from gtts import gTTS
+
+def playaudio(audio):
+    pygame.mixer.init()  # Initialize the mixer module
+    pygame.mixer.music.load(audio)  # Load the audio file
+    pygame.mixer.music.play()  # Play the audio file
+    
+    # Keep the script running until the audio finishes playing
+    while pygame.mixer.music.get_busy():
+        pygame.time.Clock().tick(10)  # Wait and prevent CPU overuse
+
+def convert_to_audio(text):
+    audio = gTTS(text=text, lang='en')
+    audio.save("textaudio.mp3")
+    playaudio("textaudio.mp3")
+
+convert_to_audio("Hello world, my name is Lakshan.")
+
